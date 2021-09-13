d39dfda654aa1a6157af94707ae677ad987dee0d
ItAcHlll <72550877+ItAcHlll@users.noreply.github.com>
2021-09-13 15:28:16 +0200

    wsertg

--- helloword.js
+++ helloword.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +10,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
+
+
+console.log('First');
+console.log('Second');
+setTimeout(function() {
+    console.log('First');
+    }, 8080);
+ console.log('Second');
+ 
